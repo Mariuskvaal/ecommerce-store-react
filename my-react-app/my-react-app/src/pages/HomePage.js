8ccbf1b464336714ac771595f33d737c06ef3807
Mariuskvaal <mariuskvaal1@gmail.com>
2024-09-10 16:43:13 +0200

    Trying t fix errors

---
+++ my-react-app/my-react-app/src/pages/HomePage.js
@@ -0,0 +1,13 @@
+// src/pages/HomePage.js
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to the HomePage</h1>
+    </div>
+  );
+}
+
+export default HomePage; // Ensure this is correct
+
